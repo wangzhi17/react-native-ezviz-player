814eaba02d3a008df6162cd226fd7ff91b9da630
wangzhi <wangzhi@cainiaomeishi.com>
2024-07-17 22:40:52 +0800

    兼容react native0.73和react native0.74.3，这个版本将不支持0.74.2（android 0.74.2自动生成的父类数据类型不一致）

--- android/src/main/java/cn/wangzhi17/ezviz/player/RCTEzvizPlayerModule.java
+++ android/src/main/java/cn/wangzhi17/ezviz/player/RCTEzvizPlayerModule.java
@@ -18,122 +18,122 @@ import com.videogo.openapi.EZOpenSDK;
 import com.videogo.openapi.EZPlayer;
 
 public class RCTEzvizPlayerModule extends NativeEzvizVoiceTalkSpec implements Handler.Callback {
-    private final String Name = "EzvizPlayerModule";
-    private final String voiceTalkEvent = "voiceTalk";
-    private final ReactApplicationContext reactContext;
+  private final String Name = "EzvizPlayerModule";
+  private final String voiceTalkEvent = "voiceTalk";
+  private final ReactApplicationContext reactContext;
 
-    private EZPlayer mEZPlayer;
+  private EZPlayer mEZPlayer;
 
-    private Handler mHandler;
+  private Handler mHandler;
 
-    @NonNull
-    @Override
-    public String getName() {
-        return Name;
+  @NonNull
+  @Override
+  public String getName() {
+    return Name;
+  }
+
+  @Override
+  public void initSDK(String appKey, Promise promise) {
+    Application application = (Application) reactContext.getBaseContext();
+    EZOpenSDK.initLib(application, appKey);
+    promise.resolve(true);
+  }
+
+  @Override
+  public void destroySDK(Promise promise) {
+    EZOpenSDK.finiLib();
+    promise.resolve(true);
+  }
+
+  public RCTEzvizPlayerModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public void createVoicePlayer(String deviceSerial, double cameraNo, String verifyCode, Promise promise) {
+    mEZPlayer = EZOpenSDK.getInstance().createPlayer(deviceSerial, (int) cameraNo);
+    mHandler = new Handler(this);
+    mEZPlayer.setPlayVerifyCode(verifyCode);
+    mEZPlayer.setHandler(mHandler);
+    promise.resolve(true);
+  }
+
+  @Override
+  public void startVoiceTalk(Promise promise) {
+    if (mEZPlayer == null) {
+      promise.reject("error", "对讲失败");
+      return;
     }
+    mEZPlayer.closeSound();
+    mEZPlayer.startVoiceTalk();
+    promise.resolve(true);
+  }
 
-    @Override
-    public void initSDK(String appKey, Promise promise) {
-        Application application = (Application) reactContext.getBaseContext();
-        EZOpenSDK.initLib(application, appKey);
-        promise.resolve(true);
+  @Override
+  public void stopVoiceTalk(Promise promise) {
+    if (mEZPlayer == null) {
+      promise.reject("error", "对讲失败");
+      return;
     }
+    mEZPlayer.stopVoiceTalk();
+    promise.resolve(true);
+  }
 
-    @Override
-    public void destroySDK(Promise promise) {
-        EZOpenSDK.finiLib();
-        promise.resolve(true);
+  @Override
+  public void destroyVoiceTalk(Promise promise) {
+    promise.resolve(true);
+    if (mEZPlayer == null) {
+      return;
     }
+    mEZPlayer.release();
+    mEZPlayer = null;
+    mHandler = null;
+  }
 
-    public RCTEzvizPlayerModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
+  @Override
+  public void addListener(String eventName) {
+
+  }
+
+  @Override
+  public void removeListeners(double count) {
+
+  }
+
+  @Override
+  public boolean handleMessage(@NonNull Message msg) {
+    switch (msg.what) {
+      case EZConstants.EZRealPlayConstants.MSG_REALPLAY_VOICETALK_SUCCESS:
+        if (mEZPlayer != null)
+          mEZPlayer.openSound();
+        WritableMap successMap = Arguments.createMap();
+        successMap.putString("description", "对讲成功");
+        successMap.putString("sulution", "");
+        successMap.putInt("errorCode", 0);
+        sendEvent(voiceTalkEvent, successMap);
+        break;
+
+      case EZConstants.EZRealPlayConstants.MSG_REALPLAY_VOICETALK_STOP:
+        WritableMap stopMap = Arguments.createMap();
+        stopMap.putString("description", "对讲结束");
+        stopMap.putString("sulution", "");
+        stopMap.putInt("errorCode", -1);
+        sendEvent(voiceTalkEvent, stopMap);
+        break;
+      case EZConstants.EZRealPlayConstants.MSG_REALPLAY_VOICETALK_FAIL:
+        ErrorInfo errorInfo = (ErrorInfo) msg.obj;
+        WritableMap failMap = Arguments.createMap();
+        failMap.putString("description", errorInfo.description);
+        failMap.putString("sulution", errorInfo.sulution);
+        failMap.putInt("errorCode", errorInfo.errorCode);
+        sendEvent(voiceTalkEvent, failMap);
+        break;
     }
+    return false;
+  }
 
-    @Override
-    public void createVoicePlayer(String deviceSerial, int cameraNo, String verifyCode, Promise promise) {
-        mEZPlayer = EZOpenSDK.getInstance().createPlayer(deviceSerial, cameraNo);
-        mHandler = new Handler(this);
-        mEZPlayer.setPlayVerifyCode(verifyCode);
-        mEZPlayer.setHandler(mHandler);
-        promise.resolve(true);
-    }
-
-    @Override
-    public void startVoiceTalk(Promise promise) {
-        if (mEZPlayer == null) {
-            promise.reject("error", "对讲失败");
-            return;
-        }
-        mEZPlayer.closeSound();
-        mEZPlayer.startVoiceTalk();
-        promise.resolve(true);
-    }
-
-    @Override
-    public void stopVoiceTalk(Promise promise) {
-        if (mEZPlayer == null) {
-            promise.reject("error", "对讲失败");
-            return;
-        }
-        mEZPlayer.stopVoiceTalk();
-        promise.resolve(true);
-    }
-
-    @Override
-    public void destroyVoiceTalk(Promise promise) {
-        promise.resolve(true);
-        if (mEZPlayer == null) {
-            return;
-        }
-        mEZPlayer.release();
-        mEZPlayer = null;
-        mHandler = null;
-    }
-
-    @Override
-    public void addListener(String eventName) {
-
-    }
-
-    @Override
-    public void removeListeners(int count) {
-
-    }
-
-    @Override
-    public boolean handleMessage(@NonNull Message msg) {
-        switch (msg.what) {
-            case EZConstants.EZRealPlayConstants.MSG_REALPLAY_VOICETALK_SUCCESS:
-                if (mEZPlayer != null)
-                    mEZPlayer.openSound();
-                WritableMap successMap = Arguments.createMap();
-                successMap.putString("description", "对讲成功");
-                successMap.putString("sulution", "");
-                successMap.putInt("errorCode", 0);
-                sendEvent(voiceTalkEvent, successMap);
-                break;
-
-            case EZConstants.EZRealPlayConstants.MSG_REALPLAY_VOICETALK_STOP:
-                WritableMap stopMap = Arguments.createMap();
-                stopMap.putString("description", "对讲结束");
-                stopMap.putString("sulution", "");
-                stopMap.putInt("errorCode", -1);
-                sendEvent(voiceTalkEvent, stopMap);
-                break;
-            case EZConstants.EZRealPlayConstants.MSG_REALPLAY_VOICETALK_FAIL:
-                ErrorInfo errorInfo = (ErrorInfo) msg.obj;
-                WritableMap failMap = Arguments.createMap();
-                failMap.putString("description", errorInfo.description);
-                failMap.putString("sulution", errorInfo.sulution);
-                failMap.putInt("errorCode", errorInfo.errorCode);
-                sendEvent(voiceTalkEvent, failMap);
-                break;
-        }
-        return false;
-    }
-
-    private void sendEvent(String eventName, @Nullable WritableMap params) {
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
-    }
+  private void sendEvent(String eventName, @Nullable WritableMap params) {
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+  }
 }
